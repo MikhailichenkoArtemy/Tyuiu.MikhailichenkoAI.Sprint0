fe235a672c056fc1fb21f45725ea6d77338247b1
Fox <Fox@D>
2026-09-07 15:55:55 +0500

    Add project files.

---
+++ Tyuiu.MikhailichenkoAI.Sprint0.Task0.v0/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.MikhailichenkoAI.Sprint0.Task0.v0
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
